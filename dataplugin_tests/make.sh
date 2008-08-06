9ca18ff966796e0a570ae886d366c7bda4e46db1
Ryan C. Gordon <icculus@icculus.org>
2008-08-06 11:47:26 +0200

    Added make.sh for now.

---
+++ dataplugin_tests/make.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# !!! FIXME: should probably just write a meta-makefile instead...
+set -e
+
+for feh in * ; do
+    if [ -f $feh/Makefile ]; then
+        echo $feh ...
+        cd $feh
+        make
+        cd ..
+    fi
+done
